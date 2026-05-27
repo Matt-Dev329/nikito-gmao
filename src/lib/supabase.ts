86d7205a64a57b84dec5e3e8e54cd43654115faf
Matt-Dev329 <m.monnoir@gmail.com>
2026-05-27 12:03:04 +0200

    Fix password reset auto-redirect

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -13,7 +13,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
+    detectSessionInUrl: false,
   },
   global: {
     headers: { 'x-client-info': 'nikito-gmao-web' },
